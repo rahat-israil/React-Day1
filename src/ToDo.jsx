01e3c9bb79e7c3c37223cbaaa8600be5e66c5ef2
rahat-israil <rahat.bin.israil@gmail.com>
2024-11-29 04:56:52 +0600

    Conditional Rendering with Ternary Operator

--- src/ToDo.jsx
+++ src/ToDo.jsx
@@ -9,15 +9,29 @@ import PropTypes from "prop-types";
 // };
 
 
-// Conditional Rendaring
-const ToDo = ({ task, isDone }) =>  {
-  if (isDone === true)  {
-    return <li>Finished:  {task}</li>
-  } 
-  else { 
-    return <li>Work on: {task}</li> 
-  } 
-}; 
+// Conditional Rendaring 1
+// const ToDo = ({ task, isDone }) =>  {
+//   if (isDone === true)  {
+//     return <li>Finished:  {task}</li>
+//   }
+//   else {
+//     return <li>Work on: {task}</li>
+//   }
+// };
+
+// Conditional Rendering 2
+// const ToDo = ({ task, isDone }) => {
+//   if (isDone) {
+//     return <li>Finished: {task}</li>
+//   }
+//   return <li>Work on: {task}</li>
+// };
+ 
+// Conditional Rendering 3 (Ternary Operator)
+const ToDo = ({ task, isDone }) => {
+  return <li> {isDone? 'Finished' : 'Work on'} : {task}</li>
+ };
+
 
 ToDo.propTypes = {
   task: PropTypes.object.isRequired,
