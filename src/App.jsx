259fe29cc44030f46fd01c84a58f0f513f1c6a52
rahat-israil <rahat.bin.israil@gmail.com>
2024-11-29 01:51:25 +0600

    Default Value set & pass Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,41 @@
-import './App.css'
+import './App.css';
 import PropTypes from 'prop-types';
 
 function App() {
-
   return (
     <>
-      <h1 className='text-4xl mb-20 text-center font-bold'>Day 1</h1>
-      <Device name='Rahat' age='24'></Device>
-      <Student name='Sara' age='23' grade='13' score='90'></Student>
-      <Student name='Jahan' age='24' grade='14' score='95'></Student>
+      <h1 className="text-4xl mb-20 text-center font-bold">Day 1</h1>
+      <Device name="Rahat" age="24"></Device>
+      <Student name="Sara" age="23" grade="13" score="90"></Student>
+      <Student name="Jahan" age="24" grade="14" score="95"></Student>
+      <Student></Student>
     </>
-  )
+  );
 }
 
 function Device(props) {
   return (
-    <h1>My name is {props.name} & My age is { props.age }</h1>
-  )
- }
+    <h1>
+      My name is {props.name} & My age is {props.age}
+    </h1>
+  );
+}
 
-
-function Student({ name, age, grade, score }) {
+function Student({ name = 'Asrafia', age = 25, grade = 15, score = 50 }) {
   return (
-    <div className='border-2 border-purple-900 rounded-lg p-10 w-fit mt-5'>
+    <div className="border-2 border-purple-900 rounded-lg p-10 w-fit mt-5">
       <h1>My name is {name}.</h1>
       <h2>I read in class {grade}.</h2>
       <h2>I am {age} years old.</h2>
       <h2>My average mark is {score}.</h2>
     </div>
   );
- }
+}
 
- Device.propTypes = {
-   name: PropTypes.string,
-   age: PropTypes.number,
- };
+Device.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+};
 Student.propTypes = {
   name: PropTypes.string,
   age: PropTypes.number,
@@ -42,4 +43,4 @@ Student.propTypes = {
   score: PropTypes.number,
 };
 
-export default App
+export default App;
